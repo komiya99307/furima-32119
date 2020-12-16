da44e8eb6a359c4611dbba4d600cd4b567e3950f
komiya99307 <minnminn4242@gmail.com>
2020-12-17 00:02:33 +0900

    修正機能　修正②

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,25 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze }
 
   with_options presence: true do
-  validates :nickname
-  validates :birthday
-  validates :first_name
-  validates :last_name
-  validates :first_name_kana
-  validates :last_name_kana
+    validates :nickname
+    validates :birthday
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
   end
 
   with_options format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ } do
-  validates :first_name
-  validates :last_name
+    validates :first_name
+    validates :last_name
   end
 
-  with_options format: { with: /\A[\p{katakana}]+\z/ } do
-  validates :first_name_kana
-  validates :last_name_kana
+  with_options format: { with: /\A\p{katakana}+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
   end
-
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {"test"}
-    email                 {Faker::Internet.free_email} 
-    password              {"aaa111"}
-    password_confirmation {password}
-    last_name             {"田中"}
-    first_name            {"太郎"}
-    last_name_kana        {"タナカ"}
-    first_name_kana       {"タロウ"}
-    birthday              {"1990-1-1"}
+    nickname              { 'test' }
+    email                 { Faker::Internet.free_email }
+    password              { 'aaa111' }
+    password_confirmation { password }
+    last_name             { '田中' }
+    first_name            { '太郎' }
+    last_name_kana        { 'タナカ' }
+    first_name_kana       { 'タロウ' }
+    birthday              { '1990-1-1' }
   end
 end
-  
