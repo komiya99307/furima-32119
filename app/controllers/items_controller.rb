c99df9b787b9415f6dfcd26d858c64152f61319d
komiya99307 <minnminn4242@gmail.com>
2020-12-21 00:52:28 +0900

    ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,8 +2,12 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
   private
   def item_params
-    params.require(:item).permit(:name, :text, :category_id, :state_id, :burden_id, :area_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :image, :text, :category_id, :state_id, :burden_id, :area_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,21 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :state
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :area
+  belongs_to_active_hash :shipping_date
+
+  validates :name,  presence: true
+  validates :text,  presence: true
+  validates :price, presence: true
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :state_id, numericality: { other_than: 1 }
+  validates :burden_id, numericality: { other_than: 1 }
+  validates :area_id, numericality: { other_than: 1 }
+  
+
 end
