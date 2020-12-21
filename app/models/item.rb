852c87db2479fb7d29438d8c3787c9d6013f97fd
komiya99307 <minnminn4242@gmail.com>
2020-12-21 19:50:46 +0900

    出品商品情報の保存実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,27 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   has_one_attached :image
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to_active_hash :category
   belongs_to_active_hash :state
   belongs_to_active_hash :burden
   belongs_to_active_hash :area
   belongs_to_active_hash :shipping_date
 
-  validates :name,  presence: true
-  validates :text,  presence: true
-  validates :price, presence: true
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :state_id, numericality: { other_than: 1 }
-  validates :burden_id, numericality: { other_than: 1 }
-  validates :area_id, numericality: { other_than: 1 }
+  with_options presence: true do
+    validates :name
+    validates :text
+    validates :price
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :state_id, numericality: { other_than: 1 }
+    validates :burden_id, numericality: { other_than: 1 }
+    validates :area_id, numericality: { other_than: 1 }
+    validates :shipping_date_id, numericality: { other_than: 1 }
+  end
+  
   
 
 end
