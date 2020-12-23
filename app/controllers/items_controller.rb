898f5a57e9ba86133eb6f3b9cedfeba7134bd6c1
komiya99307 <minnminn4242@gmail.com>
2020-12-23 21:00:54 +0900

    商品詳細ページへの遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,9 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show   
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :image, :text, :category_id, :state_id, :burden_id, :area_id, :shipping_date_id, :price).merge(user_id: current_user.id)
