17a5780d0593b85ffe0c7da0f270108931787d5d
komiya99307 <minnminn4242@gmail.com>
2020-12-23 20:30:58 +0900

    ダミー画像表示

--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,12 @@ ActiveRecord::Schema.define(version: 2020_12_23_081822) do
   end
 
   create_table "histories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "item_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_histories_on_item_id"
+    t.index ["user_id"], name: "index_histories_on_user_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -72,5 +76,7 @@ ActiveRecord::Schema.define(version: 2020_12_23_081822) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "histories", "items"
+  add_foreign_key "histories", "users"
   add_foreign_key "items", "users"
 end
