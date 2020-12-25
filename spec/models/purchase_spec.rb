e9c484f23c92eaf0f773f6d170c89bc44b2c6965
komiya99307 <minnminn4242@gmail.com>
2020-12-25 14:19:57 +0900

    修正　3

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Purchase, type: :model do
     @user = FactoryBot.build(:user)
     @item = FactoryBot.build(:item)
 
-    @purchase = FactoryBot.build(:purchase)
+    @purchase = FactoryBot.create(:purchase, user_id: @user.id ,item_id: @item.id)
   end
 
   describe '商品購入' do
